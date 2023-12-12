241b18d1251b0923b6f902bae84266c0e18d4cb3
miyoung <noonchicat@gmail.com>
2023-12-12 22:14:10 +0900

    admin-> product 의 pagenation

--- app/(admin_route)/products/page.tsx
+++ app/(admin_route)/products/page.tsx
@@ -3,6 +3,13 @@ import ProductTable from "@components/ProductTable";
 import React from "react";
 import ProductModel from "@models/ProductModel";
 import { Product } from "@app/types";
+import { redirect } from "next/navigation";
+
+const PRODUCT_PER_PAGE = 10;
+
+interface Props {
+  searchParams: { page: string };
+}
 
 const fetchProducts = async (
   pageNo: number,
@@ -34,13 +41,22 @@ const fetchProducts = async (
   return products;
 };
 
-export default async function Products() {
-  const products = await fetchProducts(1, 10);
+export default async function Products({ searchParams }: Props) {
+  // 최초의 페이지는 '?page=1"라고 url 에 붙지 않으니 default 값을 준다.
+  const { page = "1" } = searchParams;
+  if (isNaN(+page)) return redirect("/404");
+
+  const products = await fetchProducts(+page, PRODUCT_PER_PAGE);
+  let hasMore = true;
+
+  // 매 페이지마다 10개의 상품을 fetch 해 오는데, 10개보다 적은 상품이 있는 경우 마지막이리고 인지
+  if (products.length < PRODUCT_PER_PAGE) hasMore = false;
+  else hasMore = true;
   return (
     <ProductTable
       products={products}
-      currentPageNo={10}
-      hasMore={false}
+      currentPageNo={+page}
+      hasMore={hasMore}
       showPageNavigator={true}
     />
   );
