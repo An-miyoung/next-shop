5841ca3258d647edff281482515fac730c31eab9
miyoung <noonchicat@gmail.com>
2023-12-13 11:33:20 +0900

    home-page 상품 render

--- app/(home_route)/page.tsx
+++ app/(home_route)/page.tsx
@@ -1,5 +1,49 @@
 import React from "react";
+import startDb from "../lib/db";
+import ProductModel from "../models/ProductModel";
+import GridView from "../components/GridView";
+import ProductCard from "../components/ProductCard";
 
-export default function Home() {
-  return <div>This is my page</div>;
+interface FetchedProduct {
+  id: string;
+  title: string;
+  description: string;
+  category: string;
+  thumbnail: string;
+  price: { base: number; discounted: number };
+  sale: number;
+}
+
+const fetchLatestProducts = async () => {
+  try {
+    await startDb();
+    const products = await ProductModel.find().sort("-createdAt").limit(20);
+
+    const finalProducts = products.map((product) => ({
+      id: product._id.toString(),
+      title: product.title,
+      description: product.description,
+      category: product.category,
+      thumbnail: product.thumbnail.url,
+      price: product.price,
+      sale: product.sale,
+    }));
+
+    return JSON.stringify(finalProducts);
+  } catch (error: any) {
+    console.log(error.message);
+    throw new Error(`상품목록을 읽어오는데 실패했습니다. ${error.message}`);
+  }
+};
+
+export default async function Home() {
+  const products: FetchedProduct[] = JSON.parse(await fetchLatestProducts());
+
+  return (
+    <GridView>
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </GridView>
+  );
 }
