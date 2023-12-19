d3d172c45651437f5c355475d9a0becce272ef99
miyoung <noonchicat@gmail.com>
2023-12-19 17:18:46 +0900

    error handling

---
+++ app/(admin_route)/products/error.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  const router = useRouter();
+
+  useEffect(() => {
+    console.error(error);
+  }, [error]);
+  return (
+    <div>
+      <h2>사용자의 입력을 처리하는 데 문제가 발생했습니다.</h2>
+      <button
+        onClick={() => {
+          router.refresh();
+          router.push("/");
+        }}
+        className=" underline"
+      >
+        Home 화면으로 돌아갑니다.
+      </button>
+    </div>
+  );
+}
