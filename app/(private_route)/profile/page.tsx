5200fef93d5d350e08f46817e3de8dbfcf834cca
miyoung <noonchicat@gmail.com>
2024-01-04 17:24:55 +0900

    최근 order render

--- app/(private_route)/profile/page.tsx
+++ app/(private_route)/profile/page.tsx
@@ -1,5 +1,4 @@
 import ProfileForm from "@components/ProfileForm";
-import { redirect } from "next/navigation";
 import React from "react";
 import EmailVerificationBanner from "@components/EmailVerificationBanner";
 import Link from "next/link";
@@ -7,7 +6,9 @@ import { getServerSession } from "next-auth";
 import { authConfig } from "@/auth";
 import startDb from "@lib/db";
 import UserModel from "@models/userModel";
-import LoadingProfile from "./loading";
+import OrderModel from "@models/orderModel";
+import OrderListPublic, { Orders } from "@components/OrderListPublic";
+import { PageNotFound } from "@components/404";
 
 export const fetchUserProfile = async () => {
   const session = await getServerSession(authConfig);
@@ -27,21 +28,47 @@ export const fetchUserProfile = async () => {
   };
 };
 
+const fetchLatestOrders = async () => {
+  const session = await getServerSession(authConfig);
+  if (!session?.user) return null;
+
+  const userId = session.user.id;
+  await startDb();
+  const orders = await OrderModel.find({ userId }).sort("-createdAt").limit(1);
+  if (!orders) return null;
+
+  // object 가 1개인 [] 가 return 됨으로 map 을 돌아야 한다.
+  const result: Orders[] = orders.map((order) => ({
+    id: order._id.toString(),
+    paymentStatus: order.paymentStatus,
+    date: order.createdAt.toString(),
+    total: order.totalAmount,
+    deliveryStatus: order.deliveryStatus,
+    products: order.orderItems,
+  }));
+  return JSON.stringify(result);
+};
+
 export default async function Profile() {
   const profile = await fetchUserProfile();
-  if (!profile) return redirect("/404");
+  if (!profile) return <PageNotFound />;
 
   const { id, name, email, avatar, verified } = profile;
+
+  const ordersString = await fetchLatestOrders();
+  if (!ordersString) return <PageNotFound />;
+
+  const order = JSON.parse(ordersString);
 
   return (
     <div>
       {!verified && <EmailVerificationBanner id={id} verified={verified} />}
-      <div className="md:flex p-4 space-y-4">
+      <div className="p-2 md:flex md:p-4 space-y-4">
         <div className="md:border-r md:border-gray-700 p-2 space-y-2 md:p-4 md:space-y-4">
           <ProfileForm avatar={avatar} email={email} id={id} name={name} />
         </div>
 
-        <div className="p-4 flex-1">
+        <div className="md:p-4 flex-1">
           <div className="flex items-baseline justify-between ">
             <h1 className="text-xl md:text-2xl text-blue-gray-800  font-semibold opacity-70 mb-2 md:mb-4 ">
               최근 주문내역
@@ -52,6 +79,9 @@ export default async function Profile() {
             >
               주문내역 모두보기
             </Link>
+          </div>
+          <div className="p-0">
+            <OrderListPublic orders={order} />
           </div>
         </div>
       </div>
